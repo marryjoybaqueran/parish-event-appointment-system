402dd6ab2a89e754b83444083c17be8fe6ba150b
normeibabida <babidanormei@gmail.com>
2025-04-15 00:38:55 -0700

    feat: added homepage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
+import HomePage from '@/views/auth/HomePage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomePage,
     },
   ],
 })
